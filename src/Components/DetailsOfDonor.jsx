06d5b48c1c4cd186fe9ee9fec8163005bcec80a5
Mohit_Pandey <155551509+ApolloCreates@users.noreply.github.com>
2024-10-27 20:26:12 +0530

    Update DetailsOfDonor.jsx

--- src/Components/DetailsOfDonor.jsx
+++ src/Components/DetailsOfDonor.jsx
@@ -1,32 +1,34 @@
-function DetailsOfDonor () {
+import PropTypes from 'prop-types';
+
+function DetailsOfDonor({ onClose }) {
   return (
-    <div className="h-full w-[40vw] z-40 bg-gray-100 m-auto mt-14 p-10 rounded-2xl">
+    <div className="bg-white w-[40vw] p-10 rounded-2xl shadow-lg relative">
+      <button onClick={onClose} className="absolute top-2 right-2  text-gray-500 hover:text-gray-700">
+        Back
+      </button>
       <h1 className="text-center text-3xl font-extrabold pb-10">Details</h1>
       <div className="personal text-lg font-medium">
-        <h1 className="text-2xl font-extrabold pb-5">Personal Information</h1>
-        <h2>Name of Donor : Mohit </h2>
-        <h2>Age : 20</h2>
-        <h2>Gender : Male</h2>
-        <h2>Email address : hdybfvy34@gmail.com</h2>
-        <h2>Phone No. : 8799541125</h2>
-        <h2>Address : Pilikothi,Mukhani,Haldwani,Uttarakhand</h2>
+        <h2>Name of Donor: Mohit</h2>
+        <h2>Age: 20</h2>
+        <h2>Gender: Male</h2>
+        <h2>Email: hdybfvy34@gmail.com</h2>
+        <h2>Phone: 8799541125</h2>
+        <h2>Address: Pilikothi, Mukhani, Haldwani, Uttarakhand</h2>
       </div>
-      <div className="health text-lg font-medium">
-        <h1 className="text-2xl font-extrabold py-5">
-          Medical and Health Information
-        </h1>
-        <h2>Blood Type : B+</h2>
-        <h2>Last Donation Date : 20-10-22</h2>
-        <h2>Medical Conditions : None</h2>
+      <div className="health text-lg font-medium pt-5">
+        <h2>Blood Type: B+</h2>
+        <h2>Last Donation Date: 20-10-22</h2>
+        <h2>Medical Conditions: None</h2>
       </div>
-      <div className="apply text-lg font-medium">
-        <h1 className="text-2xl font-extrabold py-5">
-          Application for Blood Donation
-        </h1>
+      <div className="apply text-lg font-medium pt-5">
         <h2>Applied for the date : 20-10-24 -- Sunday</h2>
       </div>
     </div>
   );
+}
+
+DetailsOfDonor.propTypes = {
+  onClose: PropTypes.func.isRequired,
 };
 
 export default DetailsOfDonor;
